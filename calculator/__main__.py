74d4993d2672abe31c90bbb17114ed606497e5e0
jorektheglitch <lozcontrariuz@yandex.ru>
2022-06-11 09:59:59 +0300

    Add command-line interface

---
+++ calculator/__main__.py
@@ -0,0 +1,19 @@
+from argparse import ArgumentParser
+from typing import Optional
+
+from .evaluate import evaluate
+
+
+parser = ArgumentParser(description='Calculator')
+parser.add_argument('expression', nargs='?')
+
+args = parser.parse_args()
+expression: Optional[str] = args.expression
+
+if expression is None:  # expression does not specified
+    parser.print_help()
+elif not expression:  # expression is empty string
+    print("Please specify expression string.")
+else:
+    result = evaluate(expression)
+    print(result)
